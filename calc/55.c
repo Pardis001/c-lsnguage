d76fc1b94d74850060a87c2690e74a7b90bf9e8e
김강민 <gimg50744@gmail.com>
2023-04-13 06:25:50 +0000

    2023-04-13 5~6교시 컴퓨터 프로그래밍 54쪽~58쪽

---
+++ calc/55.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main() {
+	int a = 3; // 변수선언 및 초기화
+	a = a + 6; 
+    if(a > 5){
+		printf("%d\v", a);
+	}else {
+	    printf("%d\n", a-2);
+	}
+	//intf("%d\n", a - 6);
+	}                     
